4400399c74d6acb3b6125e8a6aafb1add295109b
Lucas Kaminski <lucaskfreitas@hotmail.com>
2018-04-08 12:35:43 -0300

    threads sao criadas somente na main; comecei a logica da decodificacao

--- Lab2/ProjectFiles/exemploTIVA.c
+++ Lab2/ProjectFiles/exemploTIVA.c
@@ -7,18 +7,29 @@
  *      Allan Patrick de Souza - <allansouza@alunos.utfpr.edu.br>
  *      Guilherme Jacichen     - <jacichen@alunos.utfpr.edu.br>
  *      Jessica Isoton Sampaio - <jessicasampaio@alunos.utfpr.edu.br>
- *      Mariana Carri�o        - <mcarriao@alunos.utfpr.edu.br>
+ *      Mariana Carriao        - <mcarriao@alunos.utfpr.edu.br>
  *===========================================================================*/
 
+#include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <math.h>
+#include "TM4C129.h"
 #include "cmsis_os.h"
+#include <stdint.h>
 
 #define TAMANHO_FILA 500
 
-void imprime_mensagem(void const *args);
-osThreadDef (imprime_mensagem, osPriorityNormal, 1, 0);
+const uint32_t mensagem[] = {
+    0x5a990100, 0x6667feff, 0x76990100, 0x6867feff, 0x7b990100, 
+    0x6167feff, 0x27990100, 0x2667feff, 0x27990100, 0x4c67feff,
+    0x68990100, 0x6767feff, 0x7b990100, 0x5a67feff, 0x75990100,
+    0x5a67feff, 0x27990100, 0x5f67feff, 0x6c990100, 0x5a67feff, 
+    0x7b990100, 0x2767feff, 0x27990100, 0x4b67feff, 0x76990100,
+    0x5b67feff, 0x27990100, 0x4d67feff, 0x6f990100, 0x6867feff,
+    0x74990100, 0x5a67feff, 0x7a990100, 0x8a650200, 0x8ebffeff
+};
+const int mensagem_len = 35;
 
 //--------------------------------
 // Fila de espera de processamento
@@ -31,22 +42,24 @@ typedef struct fila {
 	bool empty;
 } fila_espera;
 
-
 //--------------------------------
-// Declara��o de vari�veis globais
+// Declaracao de variaveis globais
 //--------------------------------
 fila_espera *fila_primos, *fila_chaves;
+bool passou_teste_ultima = false;
+bool passou_teste_penultima = false;
 bool chave_valida = false;
 bool decodificado_tail = false;
 bool fim_teste_ultima = false;
 bool fim_teste_penultima = false;
 bool mensagem_decodificada = false;
 bool imprimindo_mensagem = false;
-FILE* log_file = NULL;
-int previous_thread = -1;
+int chave_anterior = 0;
+int chave_atual = 0;
+uint32_t ultima_word_decodificada, penultima_word_decodificada;
 
 //-----------------
-// Fun��es de filas
+// Funcoes de filas
 //-----------------
 void criar_fila (fila_espera *fila) {
 	fila->primeiro_elemento = 0;
@@ -81,29 +94,21 @@ int dequeue (fila_espera *fila) {
 	return elemento;
 }
 
-
 void print_log(int thread_index) {
-	/*if(previous_thread < 0) {
-		fprintf(log_file, "		thread %d, 1d", thread_index);
-		previous_thread = thread_index;
-		return;
-	}
-	fprintf(log_file, "		thread %d, :after %d, 1d", thread_index, previous_thread);
-	previous_thread = thread_index;*/
 	int i;
 	for(i = 0; i < 7000; i++);
 	return;
 }
 
-void criar_thread_imprime_mensagem () {
-	if (fim_teste_penultima && fim_teste_ultima && mensagem_decodificada && !imprimindo_mensagem) {
-		imprimindo_mensagem = true;
-		osThreadCreate(osThread(imprime_mensagem), NULL);
-	}
+uint32_t decode(const uint32_t *msg, int index) {
+	if (index%2 == 0)
+		return msg[index] - chave_atual;
+	else
+		return msg[index] + chave_atual;
 }
 
 //------------------------------------------------------------------------------------
-// Thread respons�vel por encher a fila_chaves com n�meros inteiros em ordem crescente
+// Thread responsavel por encher a fila_chaves com numeros inteiros em ordem crescente
 //------------------------------------------------------------------------------------
 void gerar_chaves(void const *args){
 	int chave = 2;
@@ -123,10 +128,9 @@ void gerar_chaves(void const *args){
 }
 osThreadDef (gerar_chaves, osPriorityNormal, 1, 0);
 
-
 //--------------------------------------------------------------------------
-// Thread respons�vel por verificar se os n�meros da fila_chaves s�o primos.
-// Se sim, esses n�meros s�o salvos na fila_primos.
+// Thread responsavel por verificar se os numeros da fila_chaves sao primos.
+// Se sim, esses numeros sao salvos na fila_primos.
 //--------------------------------------------------------------------------
 void verificar_primos (void const *args) {
 	int i, atual;
@@ -140,7 +144,7 @@ void verificar_primos (void const *args) {
 		
 		atual = dequeue (fila_chaves);
 
-		for (i = 2, tem_divisor = false; i < (atual / 2)+1; i++) {
+		for (i = 2, tem_divisor = false; i <= (int)sqrt((double) atual); i++) {
 			if (atual % i == 0) {
 				tem_divisor = true;
 				break;
@@ -155,49 +159,81 @@ void verificar_primos (void const *args) {
 }
 osThreadDef (verificar_primos, osPriorityNormal, 1, 0);
 
+//--------------------------------------------------------------------------
+// Thread responsavel por decodificar o final da mensagem, para realizar os
+// testes e decidir se e necessário decodificar o resto.
+//--------------------------------------------------------------------------
+void decodificar_tail(void const *args){
+	while (!chave_valida) {
+		if (fila_primos->empty || decodificado_tail) {
+			osThreadYield();
+			continue;
+		}
+		chave_anterior = chave_atual;
+		chave_atual = dequeue(fila_primos);
+		ultima_word_decodificada = decode(mensagem, mensagem_len - 1);
+		penultima_word_decodificada = decode(mensagem, mensagem_len - 2);
+		
+		print_log(3);
+		decodificado_tail = true;
+	}
+}
+osThreadDef (decodificar_tail, osPriorityNormal, 1, 0);
+
 void testar_penultima_word(void const *args){
-	if(decodificado_tail) {
+	while(!chave_valida) {
+		if(!decodificado_tail || fim_teste_penultima) {
+			osThreadYield();
+			continue;
+		}
+		if (penultima_word_decodificada == chave_atual / 2)
+			passou_teste_penultima = true;
+		else
+			passou_teste_penultima = false;
+		
 		print_log(4);
 		fim_teste_penultima = true;
-		criar_thread_imprime_mensagem();
 	}
 }
 osThreadDef (testar_penultima_word, osPriorityNormal, 1, 0);
 
 void testar_ultima_word(void const *args){
-	if(decodificado_tail) {
+	while(!chave_valida) {
+		if(!decodificado_tail || fim_teste_ultima) {
+			osThreadYield();
+			continue;
+		}
+		
+		if (ultima_word_decodificada == ((chave_atual * chave_atual) / chave_anterior))
+			passou_teste_ultima = true;
+		else
+			passou_teste_ultima = false;
+		
 		print_log(5);
 		fim_teste_ultima = true;
-		criar_thread_imprime_mensagem();
 	}
 }
 osThreadDef (testar_ultima_word, osPriorityNormal, 1, 0);
 
 void decodificar_mensagem(void const *args){
-	if(decodificado_tail) {
-		print_log(6);
-		mensagem_decodificada = true;
-		criar_thread_imprime_mensagem();
-	}
-}
-osThreadDef (decodificar_mensagem, osPriorityNormal, 1, 0);
-
-void decodificar_tail(void const *args){
-	while (!chave_valida) {
-		if (fila_primos->empty) {
+	int i;
+	
+	while(!chave_valida) {
+		if(!decodificado_tail || mensagem_decodificada) {
 			osThreadYield();
 			continue;
 		}
-		dequeue(fila_primos);
-		print_log(3);
-		decodificado_tail = true;
-		osThreadCreate(osThread(testar_penultima_word), NULL);
-		osThreadCreate(osThread(testar_ultima_word), NULL);
-		osThreadCreate(osThread(decodificar_mensagem), NULL);
-		osThreadTerminate(osThreadGetId());
+		
+		for (i = 0; i < mensagem_len - 2; i++) {
+			// vai ter que criar uma variável global pra armazenar a mensagem decodificada
+			// e usar malloc pq o tamanho muda pra cada mensagem.
+		}
+		
+		print_log(6);
+		mensagem_decodificada = true;
 	}
 }
-osThreadDef (decodificar_tail, osPriorityNormal, 1, 0);
+osThreadDef (decodificar_mensagem, osPriorityNormal, 1, 0);
 
 void imprime_mensagem(void const *args){
 	while (!chave_valida) {
@@ -205,19 +241,17 @@ void imprime_mensagem(void const *args){
 			osThreadYield();
 			continue;
 		}
-
+		if (passou_teste_ultima && passou_teste_penultima)
+			chave_valida = true;
+		
 		print_log(7);
-
-		imprimindo_mensagem = false;
 		decodificado_tail = false;
 		fim_teste_ultima = false;
 		fim_teste_penultima = false;
 		mensagem_decodificada = false;
-		osThreadCreate(osThread(decodificar_tail), NULL);
-		osThreadTerminate(osThreadGetId());
 	}
 }
-//osThreadDef (imprime_mensagem, osPriorityNormal, 1, 0);
+osThreadDef (imprime_mensagem, osPriorityNormal, 1, 0);
 
 int main(void) {
 	fila_chaves = (fila_espera*) malloc (sizeof (fila_espera));
@@ -229,9 +263,12 @@ int main(void) {
 	osKernelInitialize();
 	osThreadCreate(osThread(gerar_chaves), NULL);
 	osThreadCreate(osThread(verificar_primos), NULL);
-	osThreadCreate(osThread(decodificar_tail), NULL);	
+	osThreadCreate(osThread(decodificar_tail), NULL);
+	osThreadCreate(osThread(testar_penultima_word), NULL);
+	osThreadCreate(osThread(testar_ultima_word), NULL);
+	osThreadCreate(osThread(decodificar_mensagem), NULL);
+	osThreadCreate(osThread(imprime_mensagem), NULL);
 	osKernelStart();
 	
-	//fprintf(log_file, "gantt\n\ttitle Decodificacao");
 	osDelay(osWaitForever);
 }
